9dad620ac6c5ecc092ff6b9531d929b751962a94
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-01 15:28:10 -0700

    ty

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Fre
+ *
+ *Return: Evet
+ */
+int main(void)
+{
+	int i;
+	int space;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	space = '\n';
+	putchar(space);
+	return (0);
+}
+
