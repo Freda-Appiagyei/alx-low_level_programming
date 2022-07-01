5e03a2a221b39881488a17620ae1575753344744
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-01 14:38:30 -0700

    ty

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Every
+ *
+ *Return: Always
+ */
+int main(void)
+{
+	int ch;
+	int space;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	
+	space = '\n';
+      	putchar(space);
+	return (0);
+}
