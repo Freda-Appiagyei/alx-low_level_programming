508710a7d809024568d3335eba20a8ab1170a400
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-01 23:21:08 -0700

    jj

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-	int ch1;
 	int ch;
 	int space;
 
-	for (ch = 0; ch < 10; ch++)
-	{
-
+	for (ch = 0; ch < 9; ch++)
+		printf("%d, ", ch);
+	space = '\n';
+	printf("9");
+	putchar(space);
+	return (0);
+}
