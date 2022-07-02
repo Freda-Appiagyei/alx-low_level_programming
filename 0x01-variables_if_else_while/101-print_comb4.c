7911fcc8ccb8c5319b70e908e490b2958906fd7c
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-02 01:16:24 -0700

    t

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ *main - Gje
+ *
+ *Return: Ww
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	int m;
+	int n;
+	int o;
+	int p;
+	int q;
+	int r;
+	int s;
+	int t;
+	int space;
+
+	for (i = 12; i <= 19; i++)
+		printf("0%d, ", i);
+	for (j = 23; j <= 29; j++)
+		printf("0%d, ", j);
+	for (k = 34; k <= 39; k++)
+		printf("0%d, ", k);
+	for (l = 45; l <= 49; l++)
+		printf("0%d, ", l);
+	for (m = 56; m <= 59; m++)
+		printf("0%d, ", m);
+	for (n = 67; n <= 69; n++)
+		printf("0%d, ", n);
+	for (o = 78; o <= 79; o++)
+		printf("0%d, ", o);
+	printf("089, ");
+	for (p = 123; p <= 129; p++)
+		printf("%d, ", p);
+	for (q = 134; q <= 139; q++)
+		printf("%d, ", q);
+	printf("789");
+	space = '\n';
+	putchar(space);
+	return (0);
+}
