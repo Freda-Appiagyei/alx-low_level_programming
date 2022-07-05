274b914f05452b0cc0a25596793d4f741ec12d4f
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-05 15:52:07 -0700

    aaa

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	putchar("\n");
+	}
+	return (0);
+}
