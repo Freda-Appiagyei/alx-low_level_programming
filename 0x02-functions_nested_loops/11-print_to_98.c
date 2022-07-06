d7df44b0ce0c7465031ee448315e42a9d631f24f
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-06 14:33:27 -0700

    rt

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- *main - check the code
- *
+ *print_to_98 - check the code
+ *@n: paramter
  *Return: Always 0.
  */
-int main(void)
+void print_to_98(int n)
 {
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+			_putchar(i);
+	}
+	else
+	{
+	for (i = n; i <= 98; i++)
+		_putchar(i);
+	}
+	_putchar("\n");
 	return (0);
 }
-
