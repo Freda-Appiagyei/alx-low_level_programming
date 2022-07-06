f4393d988b7f7cd7a4886a31d88081aa395d05cf
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-06 01:10:09 -0700

    j

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+/**
+ *main - check code
+ *
+ *Return: Always 0
+ */
+void print_times_table(int n)
+{
+	int i = n;
+	int j;
+	int k;
+
+	if (n > 15 || n < 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+		k = 1
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+		}
+		}
+	}
+printf("%d, \n", k);
+}
