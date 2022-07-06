9dcb798f65f1c9c20e4333eec58b07d1da64e7df
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-06 14:51:15 -0700

    juy

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *print_to_98 - check the code
@@ -12,12 +13,12 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		for (i = n; i > 98; i--)
-			_putchar(i);
+			printf(i);
 	}
 	else
 	{
 	for (i = n; i <= 98; i++)
-		_putchar(i);
+		printf(i);
 	}
 	_putchar('\n');
 }
