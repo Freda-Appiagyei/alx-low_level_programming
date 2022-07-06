bbd29e3280657dfc91352ef6221eeeb73d7b4b06
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-05 22:53:30 -0700

    hh

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- *main - check the code.
+ *_isalpha - check the code.
  *
  *Return: Always 0.
  */
-int main(void)
+int _isalpha(int c)
 {
+	if ((i >= 'a' && i <= 'z' || i <= 'A' && i <= 'Z'))
+	{	
+		return (1);
+	}
 	return (0);
 }
