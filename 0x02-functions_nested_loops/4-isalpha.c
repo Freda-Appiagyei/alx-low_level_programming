4ced001698c9976a22dd539b8baa4a116555d0e2
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-06 09:45:23 -0700

    uhyj

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 
 /**
  *_isalpha - check the code.
- *
+ *@c: parameter
  *Return: Always 0.
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{	
+{
 		return (1);
 	}
 	return (0);
