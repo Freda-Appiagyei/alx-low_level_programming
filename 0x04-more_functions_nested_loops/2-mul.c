cf9d504beb9543237adf137e7612b4bd6f081a56
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-08 11:29:01 -0700

    GWrt

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,7 +10,7 @@
  *
  *Return: Always 0
  */
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	int t;
 
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int ans;
+
+	ans = mul(2, 3);
+	printf("%d", ans);
+	return (0);
+}
