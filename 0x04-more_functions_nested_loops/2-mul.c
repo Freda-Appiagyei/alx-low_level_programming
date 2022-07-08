b8275524ef1f21537dded867610ab9250f25cc5e
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-07 18:30:26 -0700

    jkiuk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *mul - Muti
+ *
+ *@a: parameter
+ *
+ *@b: paramter
+ *
+ *Return: Always 0
+ */
+int mul(int a, int b);
+{
+	int t;
+
+	t = a * b;
+	return (t);
+}
