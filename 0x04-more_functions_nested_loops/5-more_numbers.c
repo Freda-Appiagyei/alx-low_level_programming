acda13d51f3645329d6e4837d8b990fae82c1244
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-08 12:10:17 -0700

    ty6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ *more_numbers - Check the code
+ *
+ *Return: Paramter
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			printf("%d", i);
+		}
+	printf("\n");
+	}
+	printf("\n");
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -8,6 +8,7 @@ int main(void)
 	ans = mul(2, 3);
 	print_numbers();	
 	printf("%d", ans);
+	more_numbers();
 	print_most_numbers();
 	return (0);
 }
