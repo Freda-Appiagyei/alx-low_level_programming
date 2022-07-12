b58d2213150d5672d7c8238364767d77f2f55ad6
Freda Appiagyei <fredaappiagyei@gmail.com>
2022-07-12 14:19:49 -0700

    prints a string, followed by a new line,

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: prints a string, followed by a new line, to stdout.
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	printf("%c", str);
+}
